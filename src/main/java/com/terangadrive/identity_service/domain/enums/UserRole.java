9af5b9ff96c79331ce26c68a202b33f80521c82a
digitalmaith <digitalmaith@gmail.com>
2026-06-29 10:59:48 +0000

    fix: update UserRole enum values

--- src/main/java/com/terangadrive/identity_service/domain/enums/UserRole.java
+++ src/main/java/com/terangadrive/identity_service/domain/enums/UserRole.java
@@ -1,7 +1,7 @@
 package com.terangadrive.identity_service.domain.enums;
 
 public enum UserRole {
-    ROLE_DRIVER,
-    ROLE_OFFICER,
-    ROLE_ADMIN_TRANSPORT
+    DRIVER,
+    OFFICER,
+    ADMIN_TRANSPORT
 }
